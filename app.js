cd57bccffd9a0b39742c7524c7802039facea035
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-13 18:17:49 +0100

    step point: returns data for a pokemon, as well as the list of pokemons, in JSON format with a success message.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {success} = require('./helper.js')
 let pokemons = require('./mock-pokemon');
 
 const app = express()
@@ -6,14 +7,18 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello again, Express!👋'))
 
+//premier point de terminaison pour appeler un pokémon de la liste
 app.get('/api/pokemons/:id', (req,res) => {
 	const id = parseInt(req.params.id)
 	const pokemon= pokemons.find(pokemon => pokemon.id === id)
-	res.send(`voici le pokemon demandé n°${id}: ${pokemon.name}.`)
+	const message = 'Un pokémon à été trouvé:'
+	res.json(success(message, pokemon))
 })
 
+//nouveau point de terminaison , affichant l'ensemble de la liste de pokémons'
 app.get('/api/pokemons', (req,res) => {
-	res.send(`il y a ${pokemons.length} pokemons dans le pokedex, pour le moment`)
+	const message = 'voici la liste des pokémons enregistré dans votre pokédex:'
+	res.json(success(message, pokemons))
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
